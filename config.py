d96ab7e92682d7192d670c3a7508ba6e723ae33f
xiayulu <zuiaiqiansen@163.com>
2021-01-19 19:23:34 +0800

    demo app finished

---
+++ config.py
@@ -0,0 +1,34 @@
+"""
+Flask configuration.
+"""
+
+from os import  environ, path
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
+
+class Base:
+  SECRET_KEY = environ.get('SECRET_KEY')
+  SESSION_COOKIE_NAME = environ.get('SESSION_COOKIE_NAME')
+  FLASK_APP = environ.get('FLASK_APP')
+  
+class Product(Base):
+  FLASK_ENV = 'production'
+  DEBUG = False
+  TESTING = False
+  SQLALCHEMY_DATABASE_URI = environ.get('PRODUCT_DATABASE_URI')
+  SQLALCHEMY_ECHO = False
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class Dev(Base):
+  FLASK_ENV = 'development'
+  DEBUG = True
+  TESTING = True
+  SQLALCHEMY_DATABASE_URI = environ.get('DEV_DATABASE_URI')
+  SQLALCHEMY_ECHO = True
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+
+
